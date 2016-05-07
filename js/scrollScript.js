929969021ebe6067a2f342cc2cca8c6a4a41f6fc
Justin Li <ml.justin.li@gmail.com>
2016-05-07 12:03:53 -0400

    Added a picture thing, made a navigation bar that doesn't do anything, and added some text so you can scroll down... Yay.

---
+++ js/scrollScript.js
@@ -0,0 +1,27 @@
+//Should set up the navigation bar so it starts off at the bottom of the page
+var body = $("body");
+var header = $("header");
+var mainNavHeight = $(".main-nav").height();
+var padding = $(window).height() - mainNavHeight;
+
+body.css("padding-top", padding);
+header.css("height", padding);
+$(window).resize(function() {
+	padding = $(window).height() - mainNavHeight;
+	body.css("padding-top", padding);
+	header.css("height", padding);
+});
+
+//Makes navigation bar stick at top when scrolling down.
+//Yayayyyayayayya.
+var  mn = $(".main-nav");
+mns = "main-nav-scrolled";
+hdr = $('header').height();
+
+$(window).scroll(function() {
+	if( $(this).scrollTop() > hdr ) {
+		mn.addClass(mns);
+	} else {
+		mn.removeClass(mns);
+	}
+});
