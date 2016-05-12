5a15c21321f6ef7e1b6bacce6e1e27e6d078d9b9
Justin Li <ml.justin.li@gmail.com>
2016-05-12 19:38:33 -0400

    Make font size of header label change with mobile size.

--- js/scrollScript.js
+++ js/scrollScript.js
@@ -4,13 +4,16 @@ var header = $("header");
 var mainNavHeight = $(".main-nav").height();
 var padding = $(window).height() - mainNavHeight;
 
-// body.css("padding-top", padding);
-// header.css("height", padding);
-// $(window).resize(function() {
-// 	padding = $(window).height() - mainNavHeight;
-// 	body.css("padding-top", padding);
-// 	header.css("height", padding);
-// });
+/*
+Sets up navigation bar so it starts flush with the bottom of the screen
+*/
+body.css("padding-top", padding);
+header.css("height", padding);
+$(window).resize(function() {
+	padding = $(window).height() - mainNavHeight;
+	body.css("padding-top", padding);
+	header.css("height", padding);
+});
 
 //Makes navigation bar stick at top when scrolling down.
 //Yayayyyayayayya.
